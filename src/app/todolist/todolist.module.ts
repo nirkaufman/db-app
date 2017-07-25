dfce125b174fe8998a9eb2f319c83eedc0227321
nirkaufman <nirkaufman@gmail.com>
2017-07-25 10:55:29 +0300

    fix

--- src/app/todolist/todolist.module.ts
+++ src/app/todolist/todolist.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TodolistComponent } from './todolist/todolist.component';
+import { TodolistComponent } from './todolist.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
