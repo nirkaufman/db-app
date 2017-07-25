d8d36814e1f2b9b11fac8891bcd37623aeddc7d6
nirkaufman <nirkaufman@gmail.com>
2017-07-25 09:49:16 +0300

    init

---
+++ src/app/todolist/todolist/todolist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styles: []
+})
+export class TodolistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
